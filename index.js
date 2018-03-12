8e5ddc422199a5d41266eb64581c378ff95fccac
Jonas <30421456+jonaskuske@users.noreply.github.com>
2018-03-12 13:02:50 +0100

    using factory function for private internal object

--- index.js
+++ index.js
@@ -1,64 +1,80 @@
+function SwipeHandler() {
 
-export default class Swipe {
-
-
-  constructor({ element, click, passive }) {
-
-    this.ـxDown = null;
-    this.ـyDown = null;
-    this.ـactions = { r: [], l: [], u: [], d: [] };
-    this.ـelement = typeof element === 'string' ? document.querySelector(element) : element;
-
-    this.ـelement.addEventListener('touchstart', e => {
-      this.ـxDown = e.touches[0].clientX;
-      this.ـyDown = e.touches[0].clientY;
-    }, { passive: true });
-    this.ـelement.addEventListener('touchmove', e => {
-      const dir = this.ـhandleTouchMove(e);
-      this.ـactions[dir].forEach(fn => fn());
-    }, { passive: true });
-  }
-
-  left(callback) {
-    this.ـl = callback;
-    return this;
-  }
-
-  right(callback) {
-    this.ـr = callback;
-    return this;
-  }
-
-  up(callback) {
-    this.ـu = callback;
-    return this;
-  }
-
-  down(callback) {
-    this.ـd = callback;
-    return this;
-  }
-
-  ـhandleTouchMove(evt) {
-    if (!this.ـxDown || !this.ـyDown) return;
-
-    const xUp = evt.touches[0].clientX;
-    const yUp = evt.touches[0].clientY;
-    const xDiff = this.ـxDown - xUp;
-    const yDiff = this.ـyDown - yUp;
-
-    if (Math.abs(xDiff) > Math.abs(yDiff)) {
-      xDiff > 0
-        ? 'l'
-        : 'r';
+  const internal = {
+    xDown: null,
+    yDown: null,
+    actions: { right: [], left: [], up: [], down: [] },
+    handleTouchMove(evt) {
+      if (!internal.xDown || !internal.yDown) return;
+  
+      const xUp = evt.touches[0].clientX;
+      const yUp = evt.touches[0].clientY;
+      const xDiff = internal.xDown - xUp;
+      const yDiff = internal.yDown - yUp;
+      internal.xDown = null;
+      internal.yDown = null;
+  
+      if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        return xDiff > 0
+          ? 'l'
+          : 'r';
+      }
+      else {
+        return yDiff > 0
+          ? 'u'
+          : 'd';
+      }
     }
-    else {
-      yDiff > 0
-        ? 'u'
-        : 'd';
+  };
+  
+  const validateFn = fn => { if (typeof fn !== 'function') throw new Error('Passed element is not of type "Function".') };
+
+  class Swipe {
+  
+    constructor({ element, click, passive }) {
+      internal.element = typeof element === 'string' ? document.querySelector(element) : element;
+  
+      internal.element.addEventListener('touchstart', e => {
+        internal.xDown = e && e.touches && e.touches[0] && e.touches[0].clientX;
+        internal.yDown = e && e.touches && e.touches[0] && e.touches[0].clientY;
+      });
+
+      internal.element.addEventListener('touchmove', e => {
+        const dir = internal.handleTouchMove(e);
+        if (!dir) return;
+  
+        internal.actions[dir].forEach(fn => fn());
+      });
     }
-    this.ـxDown = null;
-    this.ـyDown = null;
+  
+    left(callback) {
+      validateFn(callback);
+      internal.left = callback;
+      return this;
+    }
+  
+    right(callback) {
+      validateFn(callback);
+      internal.right = callback;
+      return this;
+    }
+  
+    up(callback) {
+      validateFn(callback);
+      internal.up = callback;
+      return this;
+    }
+  
+    down(callback) {
+      validateFn(callback);
+      internal.down = callback;
+      return this;
+    }
+  
   }
+  
+  return new Swipe(...arguments);
 
 }
+
+export default SwipeHandler;
